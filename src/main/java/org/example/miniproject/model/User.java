890c7609a51095dcf849826212ecb56b711087de
vartularora45 <138357352+vartularora45@users.noreply.github.com>
2026-04-28 14:29:02 +0530

    all bugs fixed

--- src/main/java/org/example/miniproject/model/User.java
+++ src/main/java/org/example/miniproject/model/User.java
@@ -15,5 +15,6 @@ public class User {
 
     private String name;
     private String email;
-    private String password;    private String role;
+    private String password;
+    private String role; // USER or ADMIN
 }
